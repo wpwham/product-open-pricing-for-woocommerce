ee582acb6e6e3583bf0863083c4aa811a0618e9e
WP Wham <support@wpwham.com>
2021-04-19 20:00:31 -0700

    refactor and cleanup alg-wc-pop-frontend.js; add isNaN check to avoid JS warning in console

--- includes/js/alg-wc-pop-frontend.js
+++ includes/js/alg-wc-pop-frontend.js
@@ -4,13 +4,22 @@
  * @since   1.3.1
  */
 
-function alg_wc_pop_force_decimals(e){
-	e.val(parseFloat(e.val()).toFixed(alg_wc_pop_data_object.force_decimal_width));
-}
-
-jQuery(document).ready(function(){
-	jQuery(".alg_open_price").each(function(){
-		alg_wc_pop_force_decimals(jQuery(this));
-		jQuery(this).on('change',function(){alg_wc_pop_force_decimals(jQuery(this))});
+(function($){
+	
+	function alg_wc_pop_force_decimals(e){
+		var value = parseFloat( e.val() );
+		if ( isNaN( value ) ) {
+			value = 0;
+		}
+		e.val( value.toFixed( alg_wc_pop_data_object.force_decimal_width ) );
+	}
+	$( document ).ready( function(){
+		$( '.alg_open_price' ).each( function(){
+			alg_wc_pop_force_decimals( $( this ) );
+			$( this ).on( 'change', function(){
+				alg_wc_pop_force_decimals( $( this ) );
+			});
+		});
 	});
-});
+	
+})( jQuery );
