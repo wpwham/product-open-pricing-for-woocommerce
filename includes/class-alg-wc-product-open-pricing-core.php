53154e20d97f9fe0c5c3a55bffaef9a8ddf69393
Varangianoi <75825361+Varangianoi@users.noreply.github.com>
2026-03-04 22:05:07 +0300

    Fix names of the hooks

--- includes/class-alg-wc-product-open-pricing-core.php
+++ includes/class-alg-wc-product-open-pricing-core.php
@@ -747,7 +747,7 @@ class Alg_WC_Product_Open_Pricing_Core {
 			$title = get_option( 'alg_wc_product_open_pricing_label_frontend', __( 'Name Your Price', 'product-open-pricing-for-woocommerce' ) );
 
 			// Output
-			do_action( 'alg_wc_product_open_pricing_before_input_field_output', $product_id, $value, $min, $max, $is_loop );
+			do_action( 'wpwham_product_open_pricing_frontend_before_input_field', $product_id, $value, $min, $max, $is_loop );
 			
 			echo str_replace(
 				array( '%input_id%', '%open_price_input%', '%currency_symbol%', '%minimum_price%', '%max_price%', '%frontend_label%' ),
@@ -755,7 +755,7 @@ class Alg_WC_Product_Open_Pricing_Core {
 				$template
 			);
 
-			do_action( 'alg_wc_product_open_pricing_after_input_field_output', $product_id, $value, $min, $max, $is_loop );
+			do_action( 'wpwham_product_open_pricing_frontend_after_input_field', $product_id, $value, $min, $max, $is_loop );
 
 			// Disable step, if necessary
 			$step_enabled = get_option( 'alg_wc_product_open_pricing_enable_step', 'yes' );
